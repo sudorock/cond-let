b2ff357c34666f2170645f267302f9d5c35c5bcc
Sunil KS <kslvsunil@gmail.com>
2019-11-04 10:18:39 +0530

    cond-let macros initialized

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cond-let
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/cond_let/core.clj
@@ -0,0 +1,19 @@
+(ns cond-let.core)
+
+(defmacro cond-let
+  ([] nil)
+  ([cnd-bnd exp] (if (vector? cnd-bnd)
+                   (let [form (cnd-bnd 0) tst (cnd-bnd 1)]
+                     `(let [tmp# ~tst]
+                        (if tmp#
+                          (let [~form tmp#]
+                            ~exp))))
+                   `(if ~cnd-bnd ~exp)))
+  ([cnd-bnd exp & rmn] (if (vector? cnd-bnd)
+                         (let [form (cnd-bnd 0) tst (cnd-bnd 1)]
+                           `(let [tmp# ~tst]
+                              (if tmp#
+                                (let [~form tmp#]
+                                  ~exp)
+                                (cond-let ~@rmn))))
+                         `(if ~cnd-bnd ~exp (cond-let ~@rmn)))))
