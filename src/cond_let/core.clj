8e6abf91f57a1cb62cd4bbc5e316c816353ffa4b
Sunil KS <kslvsunil@gmail.com>
2019-11-04 10:35:23 +0530

    shortened code

--- src/cond_let/core.clj
+++ src/cond_let/core.clj
@@ -2,18 +2,10 @@
 
 (defmacro cond-let
   ([] nil)
-  ([cnd-bnd exp] (if (vector? cnd-bnd)
-                   (let [form (cnd-bnd 0) tst (cnd-bnd 1)]
-                     `(let [tmp# ~tst]
-                        (if tmp#
-                          (let [~form tmp#]
-                            ~exp))))
-                   `(if ~cnd-bnd ~exp)))
   ([cnd-bnd exp & rmn] (if (vector? cnd-bnd)
                          (let [form (cnd-bnd 0) tst (cnd-bnd 1)]
                            `(let [tmp# ~tst]
                               (if tmp#
-                                (let [~form tmp#]
-                                  ~exp)
+                                (let [~form tmp#] ~exp)
                                 (cond-let ~@rmn))))
                          `(if ~cnd-bnd ~exp (cond-let ~@rmn)))))
